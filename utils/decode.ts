1372a5e3473d8d4250d4668defa68f7d8acb51af
Pure-lmao <99269387+Pure-lmao@users.noreply.github.com>
2025-11-06 17:18:38 +0000

    add group titles to markets

--- utils/decode.ts
+++ utils/decode.ts
@@ -19,36 +19,36 @@ export function decodeMkt(
    mkt: number, 
    home: string = "Home",
    away: string = "Away"
-): {name: string, type: string, description: string, sides: [string, string]|[true, null], displayType: number, value?: number|string, details?: string} | undefined {
+): {name: string, type: string, description: string, groupTitle: string, sides: [string, string]|[true, null], displayType: number, value?: number|string, details?: string} | undefined {
    if(mkt == 0){
-      return {name: "Moneyline", type: "ML", description: "Moneyline", sides: [home, away], displayType: 0};
+      return {name: "Moneyline", type: "ML", description: "Moneyline", groupTitle: "Moneyline", sides: [home, away], displayType: 0};
    } else if(mkt == 1){
-      return {name: home+" Win", type: "1X2", description: "Home Win in Regular Time", sides: [home+" win", away+"/Draw"], displayType: 2};
+      return {name: home+" Win", type: "1X2", description: "Home Win in Regular Time", groupTitle: "Winner", sides: [home+" win", away+"/Draw"], displayType: 2};
    } else if(mkt == 2){
-      return {name: "Draw", type: "1X2", description: "Draw in Regular Time", sides: ["Draw", home+"/"+away], displayType: 2};
+      return {name: "Draw", type: "1X2", description: "Draw in Regular Time", groupTitle: "Winner", sides: ["Draw", home+"/"+away], displayType: 2};
    } else if(mkt == 3){
-      return {name: away+" Win", type: "1X2", description: "Away Win in Regular Time", sides: [away+" win", home+"/Draw"], displayType: 2};
+      return {name: away+" Win", type: "1X2", description: "Away Win in Regular Time", groupTitle: "Winner", sides: [away+" win", home+"/Draw"], displayType: 2};
    } else if(mkt == 4){
-      return {name: "Both Teams To Score", type: "BTTS", description: "Both Teams To Score", sides: ["Yes", "No"], displayType: 0};
+      return {name: "Both Teams To Score", type: "BTTS", description: "Both Teams To Score", groupTitle: "Both Teams To Score", sides: ["Yes", "No"], displayType: 0};
    } else if (mkt >= 10 && mkt <= 50) {
-      return {name: `Outcome ${mkt-10}`, type: "MSO", description: "Multi-Selection Outcome", value: mkt-10, sides: ["Win", "Not win"], displayType: 2};
+      return {name: `Outcome ${mkt-10}`, type: "MSO", description: "Multi-Selection Outcome", groupTitle: "Result", value: mkt-10, sides: ["Win", "Not win"], displayType: 2};
    } else if (mkt >= 100 && mkt < 300) {
-      return {name: `Handicap ${(mkt-200)/2}`, type: "AH", description: "Handicap", value: (mkt-200)/2, sides: [home, away], displayType: 1};
+      return {name: `Handicap ${(mkt-200)/2}`, type: "AH", description: "Handicap", groupTitle: "Handicap", value: (mkt-200)/2, sides: [home, away], displayType: 1};
    } else if (mkt >= 400 && mkt < 450) {
-      return {name: `Both Teams To Score (Yes) & Over ${(mkt-400)/2}`, type: "BTTS+OU", description: "Both Teams To Score (Yes) & Over/Under", value: (mkt-400)/2, sides: [true, null], displayType: 3};
+      return {name: `Both Teams To Score (Yes) & Over ${(mkt-400)/2}`, type: "BTTS+OU", description: "Both Teams To Score (Yes) & Over/Under", groupTitle: "Both Teams To Score and Over/Under", value: (mkt-400)/2, sides: [true, null], displayType: 3};
    } else if (mkt >= 450 && mkt < 500) {
-      return {name: `Both Teams To Score (Yes) & Under ${(mkt-450)/2}`, type: "BTTS+OU", description: "Both Teams To Score (Yes) & Over/Under", value: (mkt-450)/2, sides: [true, null], displayType: 3};
+      return {name: `Both Teams To Score (Yes) & Under ${(mkt-450)/2}`, type: "BTTS+OU", description: "Both Teams To Score (Yes) & Over/Under", groupTitle: "Both Teams To Score and Over/Under", value: (mkt-450)/2, sides: [true, null], displayType: 3};
    } else if (mkt >= 500 && mkt < 550) {
-      return {name: `Both Teams To Score (No) & Over ${(mkt-500)/2}`, type: "BTTS+OU", description: "Both Teams To Score (No) & Over/Under", value: (mkt-500)/2, sides: [true, null], displayType: 3};
+      return {name: `Both Teams To Score (No) & Over ${(mkt-500)/2}`, type: "BTTS+OU", description: "Both Teams To Score (No) & Over/Under", groupTitle: "Both Teams To Score and Over/Under", value: (mkt-500)/2, sides: [true, null], displayType: 3};
    } else if (mkt >= 550 && mkt < 600) {
-      return {name: `Both Teams To Score (No) & Under ${(mkt-550)/2}`, type: "BTTS+OU", description: "Both Teams To Score (No) & Over/Under", value: (mkt-550)/2, sides: [true, null], displayType: 3};
+      return {name: `Both Teams To Score (No) & Under ${(mkt-550)/2}`, type: "BTTS+OU", description: "Both Teams To Score (No) & Over/Under", groupTitle: "Both Teams To Score and Over/Under", value: (mkt-550)/2, sides: [true, null], displayType: 3};
    } else if (mkt >= 600 && mkt < 650) {
       const HTWinner = numberToSide(parseInt(mkt.toString().substring(1, 2)), home, away);//the 2nd digit
       const FTWinner = numberToSide(parseInt(mkt.toString().substring(2, 3)), home, away);//the 3rd digit
       if(HTWinner === "Unknown" || FTWinner === "Unknown"){
          throw new Error("Invalid market, 2nd and 3rd digit must be 1, 2, or 3 (Home, Draw, Away)");
       }
-      return {name: `${HTWinner}/${FTWinner}`, type: "HTFT", description: "Half Time/Full Time", value: `${HTWinner}/${FTWinner}`, sides: [true, null], displayType: 3};
+      return {name: `${HTWinner}/${FTWinner}`, type: "HTFT", description: "Half Time/Full Time", groupTitle: "Half Time/Full Time", value: `${HTWinner}/${FTWinner}`, sides: [true, null], displayType: 3};
    } else if (mkt >= 650 && mkt < 700) {
       const FTWinner = numberToSide(parseInt(mkt.toString().substring(1, 2)) - 4, home, away);//the 2nd digit
       if(FTWinner === "Unknown"){
@@ -59,30 +59,30 @@ export function decodeMkt(
          throw new Error("Invalid market, 3rd digit must be 0 or 1 (Yes, No)");
       }
       const BTTS = thirdDigit === 0 ? "Yes" : "No"
-      return {name: `${FTWinner} & ${BTTS}`, type: "FT+BTTS", description: "Full Time & Both Teams To Score", value: `${FTWinner}/${BTTS}`, sides: [true, null], displayType: 3};
+      return {name: `${FTWinner} & ${BTTS}`, type: "FT+BTTS", description: "Full Time & Both Teams To Score", groupTitle: "Full Time & Both Teams To Score", value: `${FTWinner}/${BTTS}`, sides: [true, null], displayType: 3};
    } else if (mkt >= 1000 && mkt < 2000) {
-      return {name: `Over/Under ${(mkt-1000)/2}`, type: "OU", description: "Over/Under", value: (mkt-1000)/2, sides: ["Over", "Under"], displayType: 1};
+      return {name: `Over/Under ${(mkt-1000)/2}`, type: "OU", description: "Over/Under", groupTitle: "Over/Under", value: (mkt-1000)/2, sides: ["Over", "Under"], displayType: 1};
    } else if (mkt >= 2000 && mkt < 3000) {
-      return {name: `${home} over/under ${(mkt-2000)/2}`, type: "TOU", description: "Home over/under", value: (mkt-2000)/2, sides: ["Over", "Under"], displayType: 1};
+      return {name: `${home} over/under ${(mkt-2000)/2}`, type: "TOU", description: "Home Over/Under", groupTitle: "Team Over/Under", value: (mkt-2000)/2, sides: ["Over", "Under"], displayType: 1};
    } else if (mkt >= 3000 && mkt < 4000) {
-      return {name: `${away} over/under ${(mkt-3000)/2}`, type: "TOU", description: "Away over/under", value: (mkt-3000)/2, sides: ["Over", "Under"], displayType: 1};
+      return {name: `${away} over/under ${(mkt-3000)/2}`, type: "TOU", description: "Away Over/Under", groupTitle: "Team Over/Under", value: (mkt-3000)/2, sides: ["Over", "Under"], displayType: 1};
    } else if (mkt >= 5000 && mkt < 5500) {
-      return {name: `${home} win & over ${(mkt-5000)/2}`, type: "FT+OU", description: "Home win & over/under", value: (mkt-5000)/2, sides: [true, null], displayType: 3};
+      return {name: `${home} win & over ${(mkt-5000)/2}`, type: "FT+OU", description: "Home win & Over/Under", groupTitle: "Winner & Over/Under", value: (mkt-5000)/2, sides: [true, null], displayType: 3};
    } else if (mkt >= 5500 && mkt < 6000) {
-      return {name: `${home} win & under ${(mkt-5500)/2}`, type: "FT+OU", description: "Home win & over/under", value: (mkt-5500)/2, sides: [true, null], displayType: 3};
+      return {name: `${home} win & under ${(mkt-5500)/2}`, type: "FT+OU", description: "Home win & Over/Under", groupTitle: "Winner & Over/Under", value: (mkt-5500)/2, sides: [true, null], displayType: 3};
    } else if (mkt >= 6000 && mkt < 6500) {
-      return {name: `Draw & over ${(mkt-6000)/2}`, type: "FT+OU", description: "Draw & over/under", value: (mkt-6000)/2, sides: [true, null], displayType: 3};
+      return {name: `Draw & over ${(mkt-6000)/2}`, type: "FT+OU", description: "Draw & Over/Under", groupTitle: "Winner & Over/Under", value: (mkt-6000)/2, sides: [true, null], displayType: 3};
    } else if (mkt >= 6500 && mkt < 7000) {
-      return {name: `Draw & under ${(mkt-6500)/2}`, type: "FT+OU", description: "Draw & over/under", value: (mkt-6500)/2, sides: [true, null], displayType: 3};
+      return {name: `Draw & under ${(mkt-6500)/2}`, type: "FT+OU", description: "Draw & Over/Under", groupTitle: "Winner & Over/Under", value: (mkt-6500)/2, sides: [true, null], displayType: 3};
    } else if (mkt >= 7000 && mkt < 7500) {
-      return {name: `${away} win & over ${(mkt-7000)/2}`, type: "FT+OU", description: "Away win & over/under", value: (mkt-7000)/2, sides: [true, null], displayType: 3};
+      return {name: `${away} win & over ${(mkt-7000)/2}`, type: "FT+OU", description: "Away win & Over/Under", groupTitle: "Winner & Over/Under", value: (mkt-7000)/2, sides: [true, null], displayType: 3};
    } else if (mkt >= 7500 && mkt < 8000) {
-      return {name: `${away} win & under ${(mkt-7500)/2}`, type: "FT+OU", description: "Away win & over/under", value: (mkt-7500)/2, sides: [true, null], displayType: 3};
+      return {name: `${away} win & under ${(mkt-7500)/2}`, type: "FT+OU", description: "Away win & Over/Under", groupTitle: "Winner & Over/Under", value: (mkt-7500)/2, sides: [true, null], displayType: 3};
    } else if (mkt >= 10000 && mkt < 11000) {
       //home score is the 2nd and 3rd digit, away score is the 4th and 5th digit
       const home = parseInt(mkt.toString().substring(1, 3));
       const away = parseInt(mkt.toString().substring(3, 5));
-      return {name: `Correct Score ${home}-${away}`, type: "CS", description: "Correct Score", value: `${home}-${away}`, sides: [true, null], displayType: 3};
+      return {name: `Correct Score ${home}-${away}`, type: "CS", description: "Correct Score", groupTitle: "Correct Score", value: `${home}-${away}`, sides: [true, null], displayType: 3};
    } else if (mkt >= 11000 && mkt < 65535) {
       //round mkt down to nearest 500
       let startingValue = Math.floor(mkt/500) * 500;
@@ -93,7 +93,7 @@ export function decodeMkt(
          throw new Error("Invalid player prop market, market value must be between 11000 and 65535");
       }
       const value = Math.round((mkt - startingValue)/2 * 10)/10;
-      return {name: `${playerProp.prettyName} Over/Under ${value}`, type: "PP", description: `${playerProp.prettyName}`, value: value, sides: ["Over", "Under"], displayType: 1, details: playerProp.details ?? undefined};
+      return {name: `${playerProp.prettyName} Over/Under ${value}`, type: "PP", description: `${playerProp.prettyName}`, groupTitle: "Player Prop", value: value, sides: ["Over", "Under"], displayType: 1, details: playerProp.details ?? undefined};
    } else {
       throw new Error("Invalid market, market value must be between 0 and 65535");
    }
